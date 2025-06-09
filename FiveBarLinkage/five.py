0137d337f8f3deb8b715415de8538aa5868a9483
Robert Yin <140746277+EclipseaHime017@users.noreply.github.com>
2025-06-09 21:02:43 +0800

    five.py updated
    Fix a bug that sometimes one angle becomes negative

--- FiveBarLinkage/five.py
+++ FiveBarLinkage/five.py
@@ -2,14 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class five:
-    def __init__(self, l1 = 0.1, l2 = 0.1, l3 = 0.1, l4 = 0.1, l5 = 0.1):
+    def __init__(self, l1 = 0.25, l2 = 0.25, l3 = 0.25, l4 = 0.25, l5 = 0.2):
         self.l1 = l1
         self.l2 = l2
         self.l3 = l3
         self.l4 = l4
         self.l5 = l5
-        self.X_C = 0.1
-        self.Y_C = 0.1
+        self.X_C = 0.0
+        self.Y_C = 0.4
         
     def update_position(self,X_C,Y_C):
         self.X_C = X_C
@@ -23,24 +23,34 @@ class five:
         k3 = self.l3 **2  + self.l4 **2
         k4 = 2 * self.l3 * self.l4
         
-        phi2 = np.pi - np.arccos((k1 - a) / k2)
-        phi3 = np.pi - np.arccos((b - k3) / k4)
+        phi2 = -np.arccos((a - k1) / k2)
+        phi3 = np.arccos((b - k3) / k4)
         
-        c = self.Y_C / (self.X_C + self.l5/2) * (self.l1 + self.l2 * np.cos(phi2)) - self.l2 * np.sin(phi2)
-        d = self.l1 + self.l2 * np.cos(phi2) + self.Y_C / (self.X_C + self.l5/2) * self.l2 * np.sin(phi2)
+        # c = self.Y_C / (self.X_C + self.l5/2) * (self.l1 + self.l2 * np.cos(phi2)) - self.l2 * np.sin(phi2)
+        # d = self.l1 + self.l2 * np.cos(phi2) + self.Y_C / (self.X_C + self.l5/2) * self.l2 * np.sin(phi2)
+        # phi1 = np.arctan2(c,d)
+        
+        # e = self.Y_C / (self.X_C - self.l5/2) * (self.l3 + self.l4 * np.cos(phi3)) - self.l4 * np.sin(phi3)
+        # f = self.l3 + self.l4 * np.cos(phi3) + self.Y_C / (self.X_C - self.l5/2) * self.l4 * np.sin(phi3)
+        # phi4 = np.arctan2(e,f)
+        
+        c = (-(self.X_C + self.l5/2) * self.l2 * np.sin(phi2) + self.Y_C * (self.l1 + self.l2 * np.cos(phi2)))
+        d = ((self.l1 + self.l2 * np.cos(phi2)) * (self.X_C + self.l5/2) + self.Y_C * self.l2 * np.sin(phi2))
         phi1 = np.arctan2(c,d)
         
-        e = self.Y_C / (self.X_C - self.l5/2) * (self.l3 + self.l4 * np.cos(phi3)) - self.l4 * np.sin(phi3)
-        f = self.l3 + self.l4 * np.cos(phi3) + self.Y_C / (self.X_C - self.l5/2) * self.l4 * np.sin(phi3)
+        e = (-(self.X_C - self.l5/2) * self.l3 * np.sin(phi3) + self.Y_C * (self.l4 + self.l3 * np.cos(phi3)))
+        f = ((self.l4 + self.l3 * np.cos(phi3)) * (self.X_C - self.l5/2) + self.Y_C * self.l3 * np.sin(phi3))
         phi4 = np.arctan2(e,f)
-        
         return phi1, phi2, phi3, phi4
     
 def main():
     link = five(l5 = 0.2)
-    link.update_position(0.0,np.sqrt(0.03))
+    link.update_position(0.1,0.38)
     phi1, phi2, phi3, phi4 = link.inverse_kinematics()
-    print(phi1, phi2, phi3, phi4)
+    print("phi1:", phi1 * 180 / np.pi)
+    print("phi2:", phi2 * 180 / np.pi)
+    print("phi3:", phi3 * 180 / np.pi)
+    print("phi4:", phi4 * 180 / np.pi)
     
 if __name__ == '__main__':
     main()
