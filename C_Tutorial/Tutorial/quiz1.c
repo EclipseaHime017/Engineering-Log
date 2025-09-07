eff2ceccc9d198f6f0c9f02cc1c881380b3f7d33
Robert Yin <12210226@mail.sustech.edu.cn>
2025-09-07 17:41:04 +0800

    Revert "Add files via upload"
    This reverts commit 9f39faf438a6854984f868b7bc7afa84240bec10.

--- C_Tutorial/Tutorial/quiz1.c
+++
@@ -1,7 +0,0 @@
-int main() {
-  int a = 5;
-  float a = 3.14f; 
-  //   int a;
-  //   a = 10;
-  return 0;
-}
--- C_Tutorial/Tutorial/sizeof.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  int a = 5;
-  float b = 3.14f;
-  printf("Size of int: %zu bytes\n", sizeof(a));
-  printf("Size of float: %zu bytes\n", sizeof(b));
-  return 0;
-}
